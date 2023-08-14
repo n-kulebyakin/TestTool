12db3432e7973a3d0e4395371e378c65c2059e64
n-kulebyakin <nakulebyakin@yandex.ru>
2023-08-14 11:11:43 +0300

    Добавлены комментарии, небольшие изменения

--- src/site_classes/color_dialog.py
+++ src/site_classes/color_dialog.py
@@ -19,11 +19,16 @@ OBJECT_COLORS = {
 
 
 class ObjectColorWindow(QDialog):
-    _objects_colors = None
-    _objects = None
+    """
+    Класс настройки цветов логических объектов
+    отображаемых на сцене.
+    На выход принимает словарь цветов объектов по умолчанию
+    и список логических объектов отображаемых на сцене.
+    """
 
     def __init__(self, colors, visual_objects):
         super().__init__()
+        self._changed_item = []
         self._accept = QPushButton("Accept")
         self._cancel = QPushButton("Cancel")
         self._layout = QVBoxLayout()
@@ -32,64 +37,78 @@ class ObjectColorWindow(QDialog):
         self.setGeometry(400, 400, 250, 300)
         self.setLayout(self._layout)
 
+        # Запрещаем изменение значений данных в таблице
         self._color_table.setEditTriggers(self._color_table.NoEditTriggers)
         self._color_table.setColumnCount(2)
         self._color_table.setHorizontalHeaderLabels(["Type", "Value"])
         self._objects_colors = colors
         self._objects = visual_objects
 
-        all_types = [self._objects[x].log_type for x in self._objects]
-        for key in all_types:
-            if key not in self._objects_colors:
-                self._objects_colors[key] = "#c6cdda"
-
-        self._color_table.setRowCount(len(colors))
         self._layout.addWidget(self._color_table)
-
         self._layout.addWidget(self._accept)
         self._layout.addWidget(self._cancel)
+
+        # Добавляем обработку нажатий на ячейки и кнопки
         self._color_table.cellClicked.connect(self.show_color_dialog)
-        self._cancel.clicked.connect(self.close)
-        self._changed_item = []
+        self._cancel.clicked.connect(self.hide)
         self._accept.clicked.connect(self.accept_changes)
 
     def show_color_dialog(self, row, column):
+        """
+        Функция отображения окна с палитрой цветов
+        """
+        # Если была нажата не первая колонка таблицы, то возвращаемся
         if column != 1:
-            return None
-
+            return
+        color_item = self._color_table.item(row, column)
         color = QColorDialog.getColor()
-        if color.name() != self._color_table.item(row, column).text():
-            self._color_table.item(row, column).setText(color.name())
+        color_name = color.name()
+
+        # Проверяем отличается ли выбранный цвет от текущего
+        if color_name != color_item.text():
+            color_item.setText(color)
             log_type = self._color_table.item(row, 0).text()
-            self._objects_colors[log_type] = color.name()
-            self._color_table.item(row, column).setBackground(color)
-            self._changed_item.append(row)
+            # Сохраняем выбранный цвет для типа объекта
+            self._objects_colors[log_type] = color_name
+            # Заливаем задний фон выбранным цветом
+            color_item.setBackground(color)
+            # Запоминаем тип для которого изменился тип
+            self._changed_item.append(log_type)
 
     def accept_changes(self):
-        for row in self._changed_item:
-            logical_type = self._color_table.item(row, 0).text()
-            for obj in self._objects:
-                obj_type = self._objects[obj].log_type
-                if obj_type == logical_type:
-                    color = self._objects_colors[obj_type]
-                    self._objects[obj].accept_new_colors(color)
-                    self._objects_colors[obj_type] = color
+        """
+        Функция применения изменённых цветов к объектам
+        """
+        for obj in self._objects:
+            obj_type = self._objects[obj].log_type
+            # Проверяем изменился ли цвет для данного типа
+            if obj_type == self._changed_item:
+                color = self._objects_colors[obj_type]
+                # Применяем новый цвет к объекту
+                self._objects[obj].accept_new_colors(color)
+        self._changed_item.clear()
         super().hide()
 
     def show(self) -> None:
+        # Добавляем цвет по умолчанию для всех новых типов
+        # не включенных в изначальные данных
         all_types = [self._objects[x].log_type for x in self._objects]
-        for key in all_types:
-            if key not in self._objects_colors:
-                self._objects_colors[key] = "#c6cdda"
+        for obj_type in all_types:
+            if obj_type not in self._objects_colors:
+                self._objects_colors[obj_type] = "#c6cdda"
 
+        # Выставляем количество строк равным количеству цветов
         self._color_table.setRowCount(len(self._objects_colors))
-
-        for n, key in enumerate(self._objects_colors):
-            object_type = QTableWidgetItem(key)
-            self._color_table.setItem(n, 0, object_type)
-            color_name = QTableWidgetItem(self._objects_colors[key])
-            color_name.setBackground(QColor(self._objects_colors[key]))
-            self._color_table.setItem(n, 1, color_name)
+        # Выставляем значения в таблице цветов
+        for row, obj_type in enumerate(self._objects_colors):
+            object_type = QTableWidgetItem(obj_type)
+            # Устанавливаем название типа в ячейку 0
+            self._color_table.setItem(row, 0, object_type)
+            color_name = QTableWidgetItem(self._objects_colors[obj_type])
+            # Заливаем задний фон цветом
+            color_name.setBackground(QColor(self._objects_colors[obj_type]))
+            # Устанавливаем название цвета в ячейку 1
+            self._color_table.setItem(row, 1, color_name)
         self._color_table.resizeRowsToContents()
         self._color_table.resizeColumnToContents(1)
         super().show()
